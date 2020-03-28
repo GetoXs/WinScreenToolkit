665d200c8c6a80acb8d16f748ca4ea38849c4485
Mateusz Przybyłek <getoxs@gmail.com>
2020-03-28 22:25:36 +0100

    Add project files.

---
+++ WinScreenToolkit/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+
+
+namespace WinScreenToolkit
+{
+    class Program
+    {
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            RECT rc;
+            User32.GetWindowRect(hwnd, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            User32.PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            while (true)
+            {
+                Thread.Sleep(100);
+
+                //if (GetAsyncKeyState(VK_F12))
+                //{
+                //    break;
+                //}
+
+                if (User32.GetAsyncKeyState(User32.VK_LBUTTON) != 0)
+                {
+                    POINT pt;
+
+                    if (!User32.GetCursorPos(out pt))
+                    {
+                        break;
+                    }
+
+                    var hwndPt = User32.WindowFromPoint(pt);
+                    var bmp = PrintWindow(hwndPt);
+                    bmp.Save("test.png", ImageFormat.Png);
+                }
+            }
+
+        }
+    }
+}
---
+++ WinScreenToolkit/User32.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinScreenToolkit
+{
+    static class User32
+    {
+        public const UInt16 VK_MBUTTON = 0x04;//middle mouse button
+        public const UInt16 VK_LBUTTON = 0x01;//left mouse button
+        public const UInt16 VK_RBUTTON = 0x02;//right mouse button
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(UInt16 virtualKeyCode);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(System.Drawing.Point p);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+    }
+}
